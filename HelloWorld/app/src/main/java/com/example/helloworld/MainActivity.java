513800f0b2fa399a23f6b992b0d464aa97d2196b
Fabien <nirifabien@gmail.com>
2020-11-14 16:34:06 +0200

    Adding Log message

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.helloworld;
 
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Hello World");
     }
 }
